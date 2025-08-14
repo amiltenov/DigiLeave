c454820417938437789d0b497df24eb2f6d7b726
alekmiltenov <miltenoval@digitoll.bg>
2025-08-14 16:56:42 +0300

    gitignore

--- digileave/src/main/java/com/digileave/digileave/DigileaveApplication.java
+++ digileave/src/main/java/com/digileave/digileave/DigileaveApplication.java
@@ -1,5 +1,6 @@
 package com.digileave.digileave;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DigileaveApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+        System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
+
 		SpringApplication.run(DigileaveApplication.class, args);
 	}
 
