715d439d49fc8a53b8d0cc0f1410dd21eec5d817
hristobelchinov <162201580+hristobelchinov@users.noreply.github.com>
2025-08-19 15:59:31 +0300

    requests logic

---
+++ digileave/src/main/java/com/digileave/digileave/DatabaseOps/RequestRepository.java
@@ -0,0 +1,10 @@
+package com.digileave.digileave.DatabaseOps;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.digileave.digileave.Models.Request;
+
+public interface RequestRepository extends MongoRepository<Request, String> {
+    List<Request> findByEmail(String userEmail);
+}
+
--- digileave/src/main/java/com/digileave/digileave/Models/Request.java
+++ digileave/src/main/java/com/digileave/digileave/Models/Request.java
@@ -40,8 +40,8 @@ public class Request {
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
-    public String getUserId() { return userEmail; }
-    public void setUserId(String userId) { this.userEmail = userEmail; }
+    public String getUserEmail() { return userEmail; }
+    public void setUserEmail(String userId) { this.userEmail = userEmail; }
 
     public LocalDate getStartDate() { return startDate; }
     public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
