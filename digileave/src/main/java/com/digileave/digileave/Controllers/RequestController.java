af3e9ba0453af013823812cf69b9fda1922269df
alekmiltenov <miltenoval@digitoll.bg>
2025-08-19 15:25:30 +0300

    Update RequestController.java

--- digileave/src/main/java/com/digileave/digileave/Controllers/RequestController.java
+++ digileave/src/main/java/com/digileave/digileave/Controllers/RequestController.java
@@ -1,5 +1,13 @@
 package com.digileave.digileave.Controllers;
 
+import com.digileave.digileave.DatabaseOps.UserRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/request")
 public class RequestController {
-    
+	
+
 }
