8e8eb384ddca32213a6f705d4f30ed78624c4c59
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2025-08-22 18:14:58 +0300

    This should work pls
    allow public react interactions with the server

--- digileave/src/main/java/com/digileave/digileave/Config/WebConfig.java
+++ digileave/src/main/java/com/digileave/digileave/Config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.digileave.digileave.Config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://digi-leavefrontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
