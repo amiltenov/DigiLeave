35c2c96f568fa46474c560d0136d6fee22743f69
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2025-08-25 20:59:27 +0300

    ?

--- Frontend/digileave_Frontend/src/pages/Requests.jsx
+++ Frontend/digileave_Frontend/src/pages/Requests.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/requests.css";
 
-const API = import.meta.env.VITE_API_ORIGIN || "https://digileave.onrender.com";
+const API = import.meta.env.VITE_API_ORIGIN || "https://digileave.onrender.com/";
 
 export default function Requests() {
   const [items, setItems] = useState([]);
